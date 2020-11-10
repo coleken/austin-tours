4d036f7a2d6a1928be4dfc70aa93dada603f6899
Kenneth Cole <70181196+coleken@users.noreply.github.com>
2020-11-09 23:58:33 -0600

    feat: Add main activity view binding

--- app/src/main/java/com/example/austintours/MainActivity.java
+++ app/src/main/java/com/example/austintours/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.austintours;
 
 import android.os.Bundle;
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
+import com.example.austintours.databinding.ActivityMainBinding;
 import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,11 +12,13 @@ public class MainActivity extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+    View view = binding.getRoot();
+    setContentView(view);
     TabPagerAdapter tabPagerAdapter = new TabPagerAdapter(this, getSupportFragmentManager());
-    ViewPager viewPager = findViewById(R.id.view_pager);
+    ViewPager viewPager = binding.viewPager;
     viewPager.setAdapter(tabPagerAdapter);
-    TabLayout tabs = findViewById(R.id.tabs);
+    TabLayout tabs = binding.tabs;
     tabs.setupWithViewPager(viewPager);
   }
 }
