decc5077f21524941429be8bdcbbba2ca9f0dfb3
Kenneth Cole <70181196+coleken@users.noreply.github.com>
2020-11-09 22:41:06 -0600

    feat: Update attraction class and adapter

--- app/src/main/java/com/example/austintours/Attraction.java
+++ app/src/main/java/com/example/austintours/Attraction.java
@@ -1,6 +1,36 @@
 package com.example.austintours;
 
 public class Attraction {
-  // TODO fields
-  // TODO getters and setters
+
+  private String name;
+  private String hours;
+  private String days;
+  private String address;
+
+  public Attraction() {
+    // Default constructor
+  }
+
+  public Attraction(String name, String days, String hours, String address) {
+    this.name = name;
+    this.days = days;
+    this.hours = hours;
+    this.address = address;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getDays() {
+    return days;
+  }
+
+  public String getHours() {
+    return hours;
+  }
+
+  public String getAddress() {
+    return address;
+  }
 }
--- app/src/main/java/com/example/austintours/AttractionAdapter.java
+++ app/src/main/java/com/example/austintours/AttractionAdapter.java
@@ -1,36 +1,60 @@
 package com.example.austintours;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
 
 public class AttractionAdapter extends
     RecyclerView.Adapter<AttractionAdapter.AttractionViewHolder> {
 
-  // TODO add attractions list
-  // TODO add method logic
+  private final Context CONTEXT;
+  private final ArrayList<Attraction> ATTRACTIONS;
+
+  public AttractionAdapter(Context context, ArrayList<Attraction> attractions) {
+    this.CONTEXT = context;
+    this.ATTRACTIONS = attractions;
+  }
 
   @NonNull
   @Override
   public AttractionViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-    return null;
+    View view = LayoutInflater.from(parent.getContext())
+        .inflate(R.layout.attraction_item, parent, false);
+    return new AttractionViewHolder(view);
   }
 
   @Override
   public void onBindViewHolder(@NonNull AttractionViewHolder holder, int position) {
-
+    Attraction attraction = ATTRACTIONS.get(position);
+    holder.name.setText(attraction.getName());
+    holder.days.setText(attraction.getDays());
+    holder.hours.setText(attraction.getHours());
+    holder.address.setText(attraction.getAddress());
   }
 
   @Override
   public int getItemCount() {
-    return 0;
+    return ATTRACTIONS.size();
   }
 
   public static class AttractionViewHolder extends RecyclerView.ViewHolder {
 
+    public TextView name;
+    public TextView days;
+    public TextView hours;
+    public TextView address;
+
     public AttractionViewHolder(@NonNull View attractionItem) {
       super(attractionItem);
+      name = attractionItem.findViewById(R.id.text_attraction_name);
+      days = attractionItem.findViewById(R.id.text_attraction_days);
+      hours = attractionItem.findViewById(R.id.text_attraction_hours);
+      address = attractionItem.findViewById(R.id.text_attraction_address);
     }
   }
 }
