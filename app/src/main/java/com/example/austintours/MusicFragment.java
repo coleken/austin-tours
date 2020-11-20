f1a2dee35d28851d37aa9a9b052d12223eb636aa
Kenneth Cole <70181196+coleken@users.noreply.github.com>
2020-11-20 01:44:34 -0600

    fix: Correct list item grammar

--- app/src/main/java/com/example/austintours/MusicFragment.java
+++ app/src/main/java/com/example/austintours/MusicFragment.java
@@ -70,7 +70,6 @@ public class MusicFragment extends Fragment {
    */
   private int[] photoPaths() {
     return new int[]{R.drawable.austin_city_limits, R.drawable.hole_in_the_wall,
-        R.drawable.skylark_lounge, R.drawable.austin_convention_center,
-        R.drawable.ic_launcher_background, R.drawable.ic_launcher_background};
+        R.drawable.skylark_lounge, R.drawable.austin_convention_center};
   }
 }
