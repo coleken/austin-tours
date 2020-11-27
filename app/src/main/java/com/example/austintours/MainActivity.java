2a159ece9ce781fa66f87438df52a7a821249a9a
Kenneth Cole <70181196+coleken@users.noreply.github.com>
2020-11-27 16:37:28 -0600

    refactor: Migrate to ViewPager2

--- app/src/main/java/com/example/austintours/MainActivity.java
+++ app/src/main/java/com/example/austintours/MainActivity.java
@@ -3,9 +3,10 @@ package com.example.austintours;
 import android.os.Bundle;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
 import com.example.austintours.databinding.ActivityMainBinding;
 import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +16,11 @@ public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
     View view = binding.getRoot();
     setContentView(view);
-    TabPagerAdapter tabPagerAdapter = new TabPagerAdapter(this, getSupportFragmentManager());
-    ViewPager viewPager = binding.viewPager;
+    TabPagerAdapter tabPagerAdapter = new TabPagerAdapter(this);
+    ViewPager2 viewPager = binding.viewPager;
     viewPager.setAdapter(tabPagerAdapter);
     TabLayout tabs = binding.tabs;
-    tabs.setupWithViewPager(viewPager);
+    new TabLayoutMediator(tabs, viewPager,
+        (tab, position) -> tab.setText(tabPagerAdapter.getPageTitle(position, this))).attach();
   }
 }
--- app/src/main/java/com/example/austintours/TabPagerAdapter.java
+++ app/src/main/java/com/example/austintours/TabPagerAdapter.java
@@ -7,40 +7,35 @@ import static com.example.austintours.R.string.tab_4;
 
 import android.content.Context;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 /**
- * A [FragmentPagerAdapter] that returns a fragment corresponding to one of the tabs.
+ * A [FragmentStateAdapter] that returns a fragment corresponding to one of the tabs.
  */
-public class TabPagerAdapter extends FragmentPagerAdapter {
+public class TabPagerAdapter extends FragmentStateAdapter {
 
-  private final Context CONTEXT;
   private final int[] TAB_TITLES = {tab_1, tab_2, tab_3, tab_4};
   private final Fragment[] ATTRACTIONS = new Fragment[]{new HistoryFragment(), new FoodFragment(),
       new MusicFragment(), new ParksFragment()};
 
-  public TabPagerAdapter(Context context, FragmentManager fm) {
-    super(fm);
-    this.CONTEXT = context;
+  public TabPagerAdapter(FragmentActivity fragmentActivity) {
+    super(fragmentActivity);
   }
 
   @NonNull
   @Override
-  public Fragment getItem(int position) {
+  public Fragment createFragment(int position) {
     return ATTRACTIONS[position];
   }
 
-  @Nullable
   @Override
-  public CharSequence getPageTitle(int position) {
-    return CONTEXT.getResources().getString(TAB_TITLES[position]);
+  public int getItemCount() {
+    return ATTRACTIONS.length;
   }
 
-  @Override
-  public int getCount() {
-    return ATTRACTIONS.length;
+  public CharSequence getPageTitle(int position, @NonNull Context context) {
+    return context.getResources().getString(TAB_TITLES[position]);
   }
 }
